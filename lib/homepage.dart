c90f9ff65a4c0a8bb06c868aba53d60e032aee67
Md-Sifatullah617 <sifatullah35-617@diu.edu.bd>
2023-03-25 22:10:41 +0600

    hey

---
+++ lib/homepage.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:hive_project/models/notes_models.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final titleCtrl = TextEditingController();
+  final descriptionCtrl = TextEditingController();
+  Future<void> popUpDialog() async {
+    return showDialog(context: context, builder: (context){
+      return AlertDialog(
+        title: const Text("Add Notes"),
+        content: SingleChildScrollView(
+          child: Column(
+            children: [
+              TextFormField(
+                controller: titleCtrl,
+                decoration: InputDecoration(
+                  hintText: 'Enter Title',
+                  border: OutlineInputBorder()
+                ),
+              ),
+              const SizedBox(height: 10,),
+              TextFormField(
+                controller: descriptionCtrl,
+                decoration: InputDecoration(
+                    hintText: 'Enter Description',
+                    border: OutlineInputBorder()
+                ),
+              )
+            ],
+          ),
+        ),
+        actions: [
+          TextButton(onPressed: (){
+            Navigator.pop(context);
+          }, child: const Text('Cancel')),
+          TextButton(onPressed: (){
+            final data = NotesModel(title: titleCtrl.text, description: descriptionCtrl.text);
+            Navigator.pop(context);
+          }, child: const Text('Add')),
+        ],
+      );
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Hive Flutter'),
+      ),
+      body: Column(
+        children: [
+
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(onPressed: (){
+        popUpDialog();
+      },
+      child: const Icon(Icons.add),
+      ),
+    );
+  }
+
+}
---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_project/models/notes_models.dart';
+import 'package:path_provider/path_provider.dart';
+
+import 'homepage.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  var directory = await getApplicationDocumentsDirectory();
+  Hive.init(directory.path);
+
+Hive.registerAdapter(NotesModelAdapter());
+
+await Hive.openBox<NotesModel>('notes');
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
